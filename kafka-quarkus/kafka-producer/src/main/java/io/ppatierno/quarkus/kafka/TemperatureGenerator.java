016d431d0ff8316f7d951b9196972776902dd4dc
Paolo Patierno <ppatierno@live.com>
2019-10-19 16:28:53 +0200

    Added min and max temperature configuration

--- kafka-quarkus/kafka-producer/src/main/java/io/ppatierno/quarkus/kafka/TemperatureGenerator.java
+++ kafka-quarkus/kafka-producer/src/main/java/io/ppatierno/quarkus/kafka/TemperatureGenerator.java
@@ -5,6 +5,7 @@ import java.util.concurrent.TimeUnit;
 
 import javax.enterprise.context.ApplicationScoped;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.reactive.messaging.Outgoing;
 
 import io.reactivex.Flowable;
@@ -12,11 +13,17 @@ import io.reactivex.Flowable;
 @ApplicationScoped
 public class TemperatureGenerator {
 
+    @ConfigProperty(name = "temperature.min")
+    private int minTemperature;
+
+    @ConfigProperty(name = "temperature.max")
+    private int maxTemperature;
+
     private Random random = new Random();
 
     @Outgoing("temperature")
     public Flowable<Integer> generate() {
         return Flowable.interval(5, TimeUnit.SECONDS)
-                        .map(t ->  20 + random.nextInt(25 + 1 - 20));
+                        .map(t ->  this.minTemperature + random.nextInt(this.maxTemperature + 1 - this.minTemperature));
     }
 }
